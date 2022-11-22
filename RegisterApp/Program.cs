2b1d12d2492c8c1f127c3a5eceefdc764e0386d3
Berselis Mendoza <berselismendoza@gmail.com>
2022-11-22 19:41:43 -0400

    Deploy
    -bug fixed on server IIS

--- RegisterApp/Program.cs
+++ RegisterApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.IO;
+
 namespace RegisterApp
 {
     public class Program
@@ -13,7 +15,13 @@ namespace RegisterApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+
+                    webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
+                    
                 });
+            
+
+
     }
 }
